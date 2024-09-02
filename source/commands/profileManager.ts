4ec9fc3b34feae2e0b10a7144701135f43e5779f
Mikhail Svetlov <jegnum@gmail.com>
2024-09-02 17:39:29 +0400

    fix createNewProfile, create getProfileList

--- source/commands/profileManager.ts
+++ source/commands/profileManager.ts
@@ -21,7 +21,7 @@ async function createNewProfile() {
   
   const newSsh = { "SSH": ssh }
   
-  await kv.set([name], newSsh);
+  await kv.set(["Name:", name], newSsh);
   
   console.log(`User ${name} saved successfully`);
   
@@ -38,13 +38,25 @@ async function createNewProfile() {
 async function getProfileList() {
   const kv = await Deno.openKv();
   
-  const rez = await kv.list
+  const iter = kv.list<string>({ prefix: ["Name:"] });
+  const users = [];
+  for await (const res of iter) users.push(res);
+  for (let i = 0; i < users.length; i++) {
+    console.log(users[i])
+  }
+    
   
-  console.log(rez);
+  // console.log(users[0]); // { key: ["users", "alex"], value: "alex", versionstamp: "00a44a3c3e53b9750000" }
+  // console.log(users[1]); // { key: ["users", "sam"], value: "sam", versionstamp: "00e0a2a0f0178b270000" }
+  // console.log(users[2]); // { key: ["users", "taylor"], value: "taylor", versionstamp: "0059e9035e5e7c5e0000" }
+  
+  // const rez = await kv.list;
+  
+  // console.log(rez);
   
   kv.close();
 }
 
-createNewProfile()
+// createNewProfile()
 
-// getProfileList()
+getProfileList()
