46704b04ff27476942db5675dfab72f9eb9b9a66
Mikhail Svetlov <jegnum@gmail.com>
2024-09-02 17:42:56 +0400

    Cleaned up the code

--- source/commands/profileManager.ts
+++ source/commands/profileManager.ts
@@ -9,30 +9,21 @@ async function getUserInput(prompt: string): Promise<string> {
   throw new Error("No input received");
 }
 
-
-
 async function createNewProfile() {
   const name = await getUserInput("Please enter a name:");
   const ssh = undefined;
-  
   const kv = await Deno.openKv();
-  
-  // const newUser = { "Name": name };
-  
   const newSsh = { "SSH": ssh }
   
   await kv.set(["Name:", name], newSsh);
   
   console.log(`User ${name} saved successfully`);
   
-  // const savedUser = await kv.get(["users", name]);
-  const savedUser = await kv.get([name]);
-  
-  console.log("Retrieved user:", savedUser.value);
-  console.log(savedUser)
+  // const savedUser = await kv.get([name]);
+  // console.log("Retrieved user:", savedUser.value);
+  // console.log(savedUser)
   
   kv.close();
-  
 }
 
 async function getProfileList() {
@@ -40,23 +31,15 @@ async function getProfileList() {
   
   const iter = kv.list<string>({ prefix: ["Name:"] });
   const users = [];
+  
   for await (const res of iter) users.push(res);
   for (let i = 0; i < users.length; i++) {
     console.log(users[i])
   }
     
-  
-  // console.log(users[0]); // { key: ["users", "alex"], value: "alex", versionstamp: "00a44a3c3e53b9750000" }
-  // console.log(users[1]); // { key: ["users", "sam"], value: "sam", versionstamp: "00e0a2a0f0178b270000" }
-  // console.log(users[2]); // { key: ["users", "taylor"], value: "taylor", versionstamp: "0059e9035e5e7c5e0000" }
-  
-  // const rez = await kv.list;
-  
-  // console.log(rez);
-  
   kv.close();
 }
 
 // createNewProfile()
 
-getProfileList()
+// getProfileList()
