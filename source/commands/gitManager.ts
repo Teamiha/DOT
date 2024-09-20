98231c10009907a03ca7b89cf932a36597c16362
Mikhail Svetlov <jegnum@gmail.com>
2024-09-20 19:34:42 +0400

    Blanks for gitManager

--- source/commands/gitManager.ts
+++ source/commands/gitManager.ts
@@ -1,13 +1,22 @@
 import { zsh } from "@vseplet/shelly"
 import { showActiveUser } from "./activateProfile.ts";
 
-const CURRENTDIRECTORY = Deno.cwd()
-const LOCALREPOSITORY = `${CURRENTDIRECTORY}/repository/`
+const CURRENTDIRECTORY = Deno.cwd();
+const LOCALREPOSITORY = `${CURRENTDIRECTORY}/repository/`;
+const PATHTOGITCONFIG = `${Deno.env.get("HOME")}/.ssh/DOT/config`;
 
 
 async function setCurrentUserAsLocal() {
     const activeUser = await showActiveUser()
     await zsh(`git config --local user.name "${activeUser[0]}"`);
     await zsh(`git config --local user.email "${activeUser[1]}"`);
+}
+
+async function connectLocalRepositoryToCurrentSSH() {
+
+}
+
+async function parsingGitCloneCommand() {
+    
 }
 
