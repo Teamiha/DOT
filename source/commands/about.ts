2b6c4de3191c58ee080543b2dcaa05a8ab2ead31
Mikhail_Svetlov <jegnum@gmail.com>
2024-10-03 21:40:13 +0400

    test update system

--- source/commands/about.ts
+++ source/commands/about.ts
@@ -1,8 +1,13 @@
+import localDenoJson from "../../deno.json" with { type: "json" };
+const CURRENT_VERSION = localDenoJson["version"];
+
 const aboutText = `
 Text about programm.
 Beta.
+Version: ${CURRENT_VERSION}
 `;
 
 export function about() {
   console.log(aboutText);
 }
+
